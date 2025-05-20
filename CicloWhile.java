99e18b2ff40ce77f68b3378177e75cf9099378a8
RaulLeon01 <20243rd013@utez.edu.mx>
2025-05-20 12:05:52 -0600

    Con JOption listo

--- CicloWhile.java
+++ CicloWhile.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class CicloWhile {
 
     //Objeto para leer datos por teclado
-    private static final Scanner ingreso = new Scanner(System.in);
+    //private static final Scanner ingreso = new Scanner(System.in);
 
     //Metodo para obtener un numero entero, lo recibe como argumento
     public static int obtenerNumero(String mensaje){
         int numeroTemporal;
-        System.out.print(mensaje);
-        numeroTemporal = ingreso.nextInt();
+
+        //Entrada para datos por teclado
+        String entrada = JOptionPane.showInputDialog(mensaje);
+        numeroTemporal = Integer.parseInt(entrada);
 
         return numeroTemporal;
     }
@@ -17,10 +21,10 @@ public class CicloWhile {
     //Decision de numeros
     public static void verificarNumero(int numero){
         while (numero <= 0) {
-            System.out.println("Numero negativo o cero: " + numero);
+            JOptionPane.showMessageDialog(null, "Numero negativo o cero: " + numero);
             numero = obtenerNumero("Ingresa un numero entero: ");
         }
-        System.out.println("Numero positivo: " + numero);
+        JOptionPane.showMessageDialog(null, "Numero positivo: " + numero);
     }
 
     //Metodo principal main
